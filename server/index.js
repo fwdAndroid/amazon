f72882b25f44eb9ef32c1e0d437e1068f7bf7fbb
fwdAndroid <fwdkaleem@gmail.com>
2023-03-06 03:26:38 +0500

    Mongo

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express =require("express");
+const mongoose = require("mongoose");
 const authRouter = require('./routes/auth');
 const PORT = 3000;
 const app = express();
 
 //Middleware Use TO communicate Data
 app.use(authRouter);
+
+//Connections
+mongoose.connect().then(() => {
+    console.log("Connection Established")
+}).catch(e => {
+    console.log(e)
+});
 
 app.listen(PORT,"0.0.0.0",() => {
     console.log(`connected at port ${PORT}`);
