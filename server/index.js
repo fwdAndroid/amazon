cd92e19cef06f9ddeb16ef862be72f77356cd039
fwdAndroid <fwdkaleem@gmail.com>
2023-03-06 03:56:20 +0500

    MongoDB Coneection Established

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const authRouter = require('./routes/auth');
 const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://fwdkaleem:_Zeo1234567890@cluster0.etasruq.mongodb.net/?retryWrites=true&w=majority"
 
 //Middleware Use TO communicate Data
 app.use(authRouter);
 
 //Connections
-mongoose.connect().then(() => {
+mongoose.connect(DB).then(() => {
     console.log("Connection Established")
 }).catch(e => {
     console.log(e)
