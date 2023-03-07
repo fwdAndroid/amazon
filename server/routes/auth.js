d89c5972906d4c03fcb0b8952f9ed7ac32972672
fwdAndroid <fwdkaleem@gmail.com>
2023-03-08 03:48:07 +0500

    SigniN api Part 1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 const express =require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
+
 authRouter.post("/api/signup",async (req, res) => {
     try{
         const {name , email, password} = req.body;
@@ -27,6 +28,30 @@ authRouter.post("/api/signup",async (req, res) => {
         res.status(500).json({error:e.message});
     }
 });
+//SignIn
+authRouter.post("/api/signin", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ msg: "User with this email does not exist!" });
+      }
+  
+      const isMatch = await bcryptjs.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Incorrect password." });
+      }
+  
+      const token = jwt.sign({ id: user._id }, "passwordKey");
+      //... means object destructing use to access models
+      res.json({ token, ...user._doc });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
 
 //Export this files mean able to use this files function in anyother file
 module.exports = authRouter;  
